fa077f19034d44933207771e945c5ef6cbc70658
Nattachai Laohachai <nameless41@gmail.com>
2011-07-11 21:21:00 +0700

    add login page

--- Resources/ttyl/ttyl.js
+++ Resources/ttyl/ttyl.js
@@ -1,9 +1,11 @@
 Ti.include('/ttyl/home.js');
+Ti.include('/ttyl/login.js');
 
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
 tabGroup.addTab(_home.tab);
+tabGroup.addTab(login.tab);
 
 // open tab group
 tabGroup.open();
