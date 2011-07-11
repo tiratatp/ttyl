cfa15148729bf04c61c9dc9f3078f01e40f50e7d
Nattachai Laohachai <nameless41@gmail.com>
2011-07-11 21:19:41 +0700

    login

---
+++ Resources/ttyl/login.js
@@ -0,0 +1,38 @@
+Titanium.UI.setBackgroundColor('#000');
+var login = {};
+
+login.tab = null;
+login.win = null;
+login.facebookLogin = null;
+
+login.win = Titanium.UI.createWindow({
+	title:'Login',
+	backgroundColor:'#fff'
+});
+login.tab = Titanium.UI.createTab({
+	icon:'KS_nav_views.png',
+	title:'login',
+	window:login.win
+});
+
+Titanium.Facebook.appid = '214932645217412';
+Titanium.Facebook.permissions = ['publish_stream'];
+Titanium.Facebook.addEventListener('login', function(e) {
+    if (e.success) {
+        alert('Logged in');
+    }
+});
+Titanium.Facebook.addEventListener('logout', function(e) {
+    alert('Logged out');
+});
+
+Titanium.win.add(
+	Titanium.Facebook.createLoginButton({
+		center:{x:'50%',y:'50%'},
+		left:'50%',
+		top:'50%'
+	})
+);
+
+
+
