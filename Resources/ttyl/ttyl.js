c4f325ce61977de25011576cdcf957e22c2c32a7
nuttyknot <nutty.knot@gmail.com>
2011-07-11 22:47:14 +0700

    Reorder tab

--- Resources/ttyl/ttyl.js
+++ Resources/ttyl/ttyl.js
@@ -4,8 +4,9 @@ Ti.include('/ttyl/feed/feed.js')
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-tabGroup.addTab(_home.tab);
 tabGroup.addTab(login.tab);
+tabGroup.addTab(_home.tab);
 tabGroup.addTab(feed.tab);
 // open tab group
 tabGroup.open();
+tabGroup.hide();
