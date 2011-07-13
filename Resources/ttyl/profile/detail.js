2362a76ca3c59d01a9d4ef3fc559a72c895e265a
Plub Wittawin <plub@Plubs-MacBook.local>
2011-07-13 20:09:20 +0700

    split profile window and profile detail window to each file

---
+++ Resources/ttyl/profile/detail.js
@@ -0,0 +1,29 @@
+var ProfileDetail = function()
+{
+	this.win = Titanium.UI.createWindow({
+		backgroundColor:'#fff'
+	});
+	var win = this.win;
+	
+	var table1 =  Titanium.UI.createTableView({
+		style:Titanium.UI.iPhone.TableViewStyle.GROUPED
+	});
+
+	var section1 = Titanium.UI.createTableViewSection();
+	section1.headerTitle = "Visibility";
+	var row1 = Titanium.UI.createTableViewRow({title:"Friends"});
+	var row2 = Titanium.UI.createTableViewRow({title:"Followers"});
+	row1.addEventListener('click', function(e)
+	{
+		win.close();
+	});
+	row2.addEventListener('click', function()
+	{
+		win.close();
+	});
+	section1.add(row1);
+	section1.add(row2);
+	table1.setData([section1]);
+	
+	this.win.add(table1);	
+}
--- Resources/ttyl/profile/profile.js
+++ Resources/ttyl/profile/profile.js
@@ -1,54 +1,89 @@
 //Profile
 
+Ti.include('/ttyl/profile/detail.js');
+
 //Namespace
-var profile = {};
-
-profile.win = Titanium.UI.createWindow({
-	title:'Profile',
-	backgroundColor:'#fff'
-});
-
-profile.winSocial = Titanium.UI.createWindow({
-	backgroundColor:'#fff'
-});
-	
-profile.tab = Titanium.UI.createTab({
-	icon:'KS_nav_views.png',
-	title:'Profile',
-	window:profile.win
-});
-
-
-var TheTable = Titanium.UI.createTableView({});
-
-
-
-var CustomData = [
-	{icon:'/images/icons/email.png', value:'plub101@gmail.com', visibility:'offer'},
-	{icon:'/images/icons/facebook.png', value:'Plub Wittawin', visibility:'private'},
-	{icon:'/images/icons/twitter.png', value:'plub', visibility:'public'},
-	{icon:'/images/icons/foursquare.png', value:'plub101', visibility:'private'}
-];
-
-
-
-
-var data=[];
-
-for (var i = 0; i <= CustomData.length - 1; i++){
-
-	var row = Titanium.UI.createTableViewRow();
-
-	var icon =  Titanium.UI.createImageView({
-		image:CustomData[i].icon,
-		width:32,
-		height:32,
-		left:4,
-		top:9
+var Profile = function()
+{
+	this.win = Titanium.UI.createWindow({
+		title:'Profile',
+		backgroundColor:'#fff'
+	});
+		
+	this.tab = Titanium.UI.createTab({
+		icon:'KS_nav_views.png',
+		title:'Profile',
+		window:this.win
 	});
 	
+	
+	var TheTable = Titanium.UI.createTableView({});
+	
+	
+	
+	var CustomData = [
+		{icon:'/images/icons/email.png', value:'plub101@gmail.com', visibility:'offer'},
+		{icon:'/images/icons/facebook.png', value:'Plub Wittawin', visibility:'private'},
+		{icon:'/images/icons/twitter.png', value:'plub', visibility:'public'},
+		{icon:'/images/icons/foursquare.png', value:'plub101', visibility:'private'}
+	];
+	
+	
+	
+	
+	var data=[];
+	
+	for (var i = 0; i <= CustomData.length - 1; i++){
+	
+		var row = Titanium.UI.createTableViewRow();
+	
+		var icon =  Titanium.UI.createImageView({
+			image:CustomData[i].icon,
+			width:32,
+			height:32,
+			left:4,
+			top:9
+		});
+		
+		var value = Titanium.UI.createLabel({
+			text:CustomData[i].value,
+			font:{fontSize:16,fontWeight:'bold'},
+			width:'auto',
+			textAlign:'left',
+			top:13,
+			left:40,
+			height:24
+		});
+		
+		var visibility =  Titanium.UI.createLabel({
+			text:CustomData[i].visibility,
+			font:{fontSize:12},
+			width:'auto',
+			textAlign:'left',
+			top:13,
+			right:30,
+			height:24
+		});
+		
+		row.add(icon);
+		row.add(value);
+		row.add(visibility);
+	
+		row.hasChild=true;
+		row.addEventListener('click', function()
+		{
+			openSocialDetailWin(value.text);
+		});
+		row.className = 'profile_row';
+	
+	
+		data.push(row);
+	};
+	
+	var addRow = Titanium.UI.createTableViewRow();
+	
 	var value = Titanium.UI.createLabel({
-		text:CustomData[i].value,
+		text:"Add More",
 		font:{fontSize:16,fontWeight:'bold'},
 		width:'auto',
 		textAlign:'left',
@@ -56,82 +91,18 @@ for (var i = 0; i <= CustomData.length - 1; i++){
 		left:40,
 		height:24
 	});
+		
+	addRow.add(value);
 	
-	var visibility =  Titanium.UI.createLabel({
-		text:CustomData[i].visibility,
-		font:{fontSize:12},
-		width:'auto',
-		textAlign:'left',
-		top:13,
-		right:30,
-		height:24
-	});
+	data.push(addRow);
 	
-	row.add(icon);
-	row.add(value);
-	row.add(visibility);
-
-	row.hasChild=true;
-	row.addEventListener('click', function()
+	function openSocialDetailWin(value)
 	{
-		openSocialDetailWin(value.text);
-	});
-	row.className = 'profile_row';
-
-
-	data.push(row);
-};
-
-var addRow = Titanium.UI.createTableViewRow();
-
-var value = Titanium.UI.createLabel({
-	text:"Add More",
-	font:{fontSize:16,fontWeight:'bold'},
-	width:'auto',
-	textAlign:'left',
-	top:13,
-	left:40,
-	height:24
-});
+		var profileDetail = new ProfileDetail();
+		tabGroup.activeTab.open(profileDetail.win);
+	}
 	
-addRow.add(value);
-
-data.push(addRow);
-
-
-function openSocialDetailWin(value)
-{
+	TheTable.setData(data);
 	
-	var table1 =  Titanium.UI.createTableView({
-		style:Titanium.UI.iPhone.TableViewStyle.GROUPED
-	});
-	
-	var section1 = Titanium.UI.createTableViewSection();
-	section1.headerTitle = "Visibility";
-	var row1 = Titanium.UI.createTableViewRow({title:"Public"});
-	var row2 = Titanium.UI.createTableViewRow({title:"Offer"});
-	var row3 = Titanium.UI.createTableViewRow({title:"Private"});
-	row1.addEventListener('click', function()
-	{
-		profile.winSocial.close();
-	});
-	row2.addEventListener('click', function()
-	{
-		profile.winSocial.close();
-	});
-	row3.addEventListener('click', function()
-	{
-		profile.winSocial.close();
-	});
-	section1.add(row1);
-	section1.add(row2);
-	section1.add(row3);
-	table1.setData([section1]);
-	profile.winSocial.add(table1);
-	//Titanium.UI.currentTab.open(profile.winSocial);
-	tabGroup.activeTab.open(profile.winSocial);
+	this.win.add(TheTable);
 }
-
-TheTable.setData(data);
-
-profile.win.add(TheTable);
