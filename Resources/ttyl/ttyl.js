776df39e67ed00785e5a58ea51400b8d752e634f
nuttyknot <nutty.knot@gmail.com>
2011-07-17 23:45:19 +0700

    Add CouchDB client Create new user when logged in via Facebook

--- Resources/ttyl/ttyl.js
+++ Resources/ttyl/ttyl.js
@@ -1,17 +1,37 @@
+Ti.include('/ttyl/utils/json2.js');
+Ti.include('/ttyl/db/db.js');
+
 Ti.include('/ttyl/home/home.js');
 Ti.include('/ttyl/login/login.js');
-Ti.include('/ttyl/feed/feed.js')
-Ti.include('/ttyl/profile/profile.js')
+Ti.include('/ttyl/feed/feed.js');
+Ti.include('/ttyl/profile/profile.js');
 // create tab group
 // var loginTabGroup = Titanium.UI.createTabGroup();
 var tabGroup = Titanium.UI.createTabGroup();
 
 // loginTabGroup.addTab(login.tab);
+/*
+(function() {
+	var test = {};
+	test.success = true;
+	test.uid = 579110621;
+	_db.onLoggedIn(test);
+})();
+*/
 
-login.setOnLoggedInCallback(function() {
-	// loginTabGroup.hide();
-	login.win.close();
-	tabGroup.open();
+login.setOnLoggedInCallback(function(e) {
+	login.loginButton.hide();
+	Titanium.API.info(' setOnLoggedInCallback : ' + JSON.stringify(e));
+	if(e.type == "login") {
+		e.uid = Titanium.Facebook.uid;
+	}
+	if (e.uid) {
+		// loginTabGroup.hide();
+		_db.onLoggedIn(e, function() {
+			login.win.close();
+			tabGroup.open();
+		});		
+	}	
 });
 
 if(Titanium.Facebook.loggedIn) {
